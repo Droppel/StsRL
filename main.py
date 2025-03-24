659bcf59c9f917cdd8ca53cd202f2d4b8cab7656
DoppelRR <leagueoflegends@sofortstart.de>
2025-03-24 15:20:00 +0100

    Implemented rendering

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from stable_baselines3.common.logger import configure
 from episode_end_callback import EpisodeEndCallback
 
 render = False
-disable_wandb = False
+disable_wandb = True
 experiment = "CurrentTurn"
 
 if not disable_wandb:
@@ -20,7 +20,7 @@ env = CombatEnv(render=render)
 check_env(env, warn=True)
 model = PPO("MultiInputPolicy", env)
 
-# model = PPO.load(f"experiments/{experiment}/rl_model_10200000_steps.zip", env=env)
+model = PPO.load(f"experiments/{experiment}/rl_model_320000_steps.zip", env=env)
 
 checkpoint_on_event = CheckpointCallback(save_freq=1, save_path=f"experiments/{experiment}/")
 # event_callback = EveryNTimesteps(n_steps=10000, callback=checkpoint_on_event)
@@ -41,13 +41,14 @@ logger = configure(folder=f"experiments/{experiment}/", format_strings=["stdout"
 model.set_logger(logger)
 
 # Define and Train the agent
-# if show:
-#     obs, info = env.reset()
-#     while True:
-#         action, states = model.predict(obs, deterministic=True)
-#         obs, rewards, terminated, truncated, info = env.step(action)
-#         env.render(obs["board"].reshape(tetris_height, tetris_width))
-#         if terminated:
-#             obs, info = env.reset()
+obs, info = env.reset()
+while True:
+    action, states = model.predict(obs, deterministic=True)
+    env.combat.print()
+    print(action)
+    obs, rewards, terminated, truncated, info = env.step(action)
+    input()
+    if terminated:
+        obs, info = env.reset()
 
 model.learn(total_timesteps=100000000, progress_bar=True, callback=callbacks)
